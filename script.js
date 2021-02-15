0663c776cac4ff1e1a2f5b0af16689d0edbd2b1b
akankshaudakhe137 <akankshaudakhe137@gmail.com>
2021-02-15 23:22:38 +0530

    landing page design

---
+++ script.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     $("#layout_1_iphone").animate({
+//       opacity: '1',
+//       height: '410px',
+//       width: '250px'
+    
+//   });
+// });
+
+$(document).ready(function(){
+    AOS.init({
+        duration: 1200,
+    })
+});
